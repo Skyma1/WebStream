55c5a13fde7a35e29eb193e31db21b46f00b502f
ShamovL <l.shamov@kodeks.ru>
2025-11-13 14:00:46 +0300

    1

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Устанавливаем umask для создания файлов с правами 0644 (rw-r--r--)
-# Это позволит всем читать файлы, созданные nginx
-umask 0022
-
 # Создаем директории с правильными правами
 mkdir -p /var/www/streams/hls /var/www/streams/recordings
 chmod -R 755 /var/www/streams
+
+# Запускаем скрипт мониторинга прав в фоне
+/fix-permissions.sh &
 
 # Запускаем nginx
 exec nginx -g "daemon off;"
